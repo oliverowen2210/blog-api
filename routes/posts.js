969da2df565aaa0b2988a457fe5e529ce9f6e66a
oliverowen2210 <oliverowen2210@gmail.com>
2023-01-13 16:51:15 -0600

    added express-validator to posts route

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
 const Post = require("../models/post");
 const Comment = require("../models/comment");
