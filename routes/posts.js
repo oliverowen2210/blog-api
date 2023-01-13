35ec04dde17ed981e2576f7cb7ec0133f9919df7
oliverowen2210 <oliverowen2210@gmail.com>
2023-01-13 17:54:25 -0600

    added some basic error handling to posts routes

--- routes/posts.js
+++ routes/posts.js
@@ -9,31 +9,41 @@ const async = require("async");
 
 /* GET home page. */
 router.get("/", async (req, res, next) => {
-  const posts = await Post.findAll({
-    where: {
-      published: true,
-    },
-  });
+  try {
+    const posts = await Post.findAll({
+      where: {
+        published: true,
+      },
+    });
 
-  if (!posts) {
-    err = new Error("No posts were found.");
-    err.status = 404;
+    if (!posts) {
+      err = new Error("No posts were found.");
+      err.status = 404;
+      return next(err);
+    }
+  } catch (err) {
     return next(err);
   }
   res.json(posts);
 });
 
 router.get("/:postid", async (req, res, next) => {
-  const [post, comments] = await Promise.all([
-    Post.findOne({ where: { id: parseInt(req.params.postid), published: true } }),
-    Comment.findAll({
-      where: { postid: req.params.postid },
-    }),
-  ]);
+  try {
+    const [post, comments] = await Promise.all([
+      Post.findOne({
+        where: { id: parseInt(req.params.postid), published: true },
+      }),
+      Comment.findAll({
+        where: { postid: req.params.postid },
+      }),
+    ]);
 
-  if (!post) {
-    const err = new Error("No post with that id was found.");
-    err.status = 404;
+    if (!post) {
+      const err = new Error("No post with that id was found.");
+      err.status = 404;
+      return next(err);
+    }
+  } catch (err) {
     return next(err);
   }
 
@@ -44,22 +54,26 @@ router.post("/", [
   body("title", "Post title is invalid.").trim().isLength({ min: 1, max: 100 }),
   body("text", "Post text is invalid").trim().notEmpty(),
   async (req, res, next) => {
-    const errors = validationResult(req);
+    try {
+      const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-      return next(errors);
+      if (!errors.isEmpty()) {
+        return next(errors);
+      }
+
+      let published = req.body.published ? true : false;
+
+      const postData = {
+        title: req.body.title,
+        text: req.body.text,
+        published,
+      };
+
+      await Post.sync();
+      const result = await Post.create(postData);
+    } catch (err) {
+      return next(err);
     }
-
-    let published = req.body.published ? true : false;
-
-    const postData = {
-      title: req.body.title,
-      text: req.body.text,
-      published,
-    };
-
-    await Post.sync();
-    const result = await Post.create(postData);
     res.json({
       message: "Post added successfully.",
       post: {
