e32a709c7d4a0d6fde97ed50f44a5d0a08947eb6
oliverowen2210 <oliverowen2210@gmail.com>
2023-01-14 09:10:53 -0600

    added local and jwt strategies

--- app.js
+++ app.js
@@ -26,6 +26,43 @@ const postRouter = require("./routes/posts");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+//Validates email/password combo
+passport.use(
+  "local",
+  new LocalStrategy(
+    { usernameField: "email", passwordField: "password" },
+    async (email, password, done) => {
+      //look for user
+      const user = await User.findOne({ where: { email: email } });
+      if (!user)
+        return done(null, false, {
+          message: "Incorrect username or password.",
+        });
+      //check if password matches stored hash
+      const [err, result] = await bcrypt.compare(password, user.password);
+      if (err) {
+        return done(err, false, { message: "Incorrect username or password." });
+      }
+
+      return done(null, user, { message: "Sign in successful" });
+    }
+  )
+);
+
+// Validates JWT
+passport.use(
+  "jwt",
+  new JWTStrategy(
+    {
+      secretOrKey: process.env.JWT_SECRET,
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    },
+    async (token, done) => {
+      if (token) return done(null, token.user);
+    }
+  )
+);
+
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
