3295a7c7e7bc3b162e62698e51e846a88501c924
oliverowen2210 <oliverowen2210@gmail.com>
2023-01-14 09:09:33 -0600

    added user model

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const db = require("../sequelize");
+const { DataTypes } = require("sequelize");
+
+const User = db.define("User", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  isAdmin: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
