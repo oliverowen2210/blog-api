997ac8ecc4404f79bbcd28d59b4f9a3f58d3e321
oliverowen2210 <oliverowen2210@gmail.com>
2023-01-13 18:10:34 -0600

    added cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -15,6 +16,7 @@ const postRouter = require("./routes/posts");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
